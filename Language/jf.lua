8460b222fa86912fea59b4073a194e5f760a4394
Juijote <22154581+Juijote@users.noreply.github.com>
2021-06-06 08:50:51 +0800

    add chinese translation

---
+++ Language/jf.lua
@@ -0,0 +1,57 @@
+--tooltips
+SafeAddString(SKYS_KNOWN, "已收集", 1)
+
+SafeAddString(SKYS_MOREINFO1, "城镇", 1)
+SafeAddString(SKYS_MOREINFO2, "洞穴", 1)
+SafeAddString(SKYS_MOREINFO3, "公共副本", 1)
+SafeAddString(SKYS_MOREINFO4, "地下", 1)
+SafeAddString(SKYS_MOREINFO5, "组队洞穴", 1)
+
+SafeAddString(SKYS_SET_WAYPOINT, "设置天空碎片路径点", 1)
+
+--settings menu header
+SafeAddString(SKYS_TITLE, "天空碎片", 1)
+
+--appearance
+SafeAddString(SKYS_PIN_TEXTURE, "选择地图标记图标", 1)
+SafeAddString(SKYS_PIN_TEXTURE_DESC, "选择地图标记图标", 1)
+SafeAddString(SKYS_PIN_SIZE, "标记尺寸", 1)
+SafeAddString(SKYS_PIN_SIZE_DESC, "设置地图标记的尺寸", 1)
+SafeAddString(SKYS_PIN_LAYER, "标记图层", 1)
+SafeAddString(SKYS_PIN_LAYER_DESC, "当地图标记与其他图标处于同一坐标时，设置地图标记的图层。", 1)
+
+--compass
+SafeAddString(SKYS_COMPASS_UNKNOWN, "在罗盘上显示天空碎片", 1)
+SafeAddString(SKYS_COMPASS_UNKNOWN_DESC, "在罗盘上 显示/隐藏 未收集的天空碎片图标", 1)
+SafeAddString(SKYS_COMPASS_DIST, "最大标记距离", 1)
+SafeAddString(SKYS_COMPASS_DIST_DESC, "标记在罗盘上出现的最大距离", 1)
+
+SafeAddString(SKYS_MAINWORLD, "世界地图上的天空碎片颜色", 1)
+SafeAddString(SKYS_MAINWORLD_DESC, "直接在世界地图上显示的天空碎片标记的颜色", 1)
+
+--skill panel
+SafeAddString(SKYS_SKILLS, "在技能面板上回顾", 1)
+SafeAddString(SKYS_SKILLS_DESC, "选择在技能面板上显示的天空碎片计数格式", 1)
+SafeAddString(SKYS_SKILLS_OPTION1, "基础", 1)
+SafeAddString(SKYS_SKILLS_OPTION3, "高级", 1)
+SafeAddString(SKYS_SKILLS_OPTION2, "明细", 1)
+
+--filters
+SafeAddString(SKYS_UNKNOWN, "显示未知天空碎片", 1)
+SafeAddString(SKYS_UNKNOWN_DESC, "在地图上 显示/隐藏 未知天空碎片图标", 1)
+SafeAddString(SKYS_COLLECTED, "显示已收集天空碎片", 1)
+SafeAddString(SKYS_COLLECTED_DESC, "在地图上 显示/隐藏 已收集的天空碎片图标", 1)
+
+--worldmap filters
+SafeAddString(SKYS_FILTER_UNKNOWN, "未知天空碎片", 1)
+SafeAddString(SKYS_FILTER_COLLECTED, "已收集天空碎片", 1)
+
+-- Immersive Mode
+SafeAddString(SKYS_IMMERSIVE, "开启沉浸模式基于", 1)
+SafeAddString(SKYS_IMMERSIVE_DESC, "基于以下目标的完成情况，未知的天空碎片将不会显示在您正在查看的当前区域上", 1)
+
+SafeAddString(SKYS_IMMERSIVE_CHOICE1, "关闭", 1)
+SafeAddString(SKYS_IMMERSIVE_CHOICE2, "区域主线任务", 1)
+SafeAddString(SKYS_IMMERSIVE_CHOICE3, GetString(SI_MAPFILTER8), 1)
+SafeAddString(SKYS_IMMERSIVE_CHOICE4, GetAchievementCategoryInfo(6), 1)
+SafeAddString(SKYS_IMMERSIVE_CHOICE5, "区域任务", 1)
